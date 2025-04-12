0a635554ce42fa3aa22c39dcc85351b289775226
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2025-04-12 09:57:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+window.onload = function () {
+  const button = document.querySelector('input[type="button"]');
+  const select = document.getElementById('colorSelect');
+
+  button.addEventListener('click', () => {
+    // Get the selected index
+    const selectedIndex = select.selectedIndex;
+
+    // Check if any option is selected
+    if (selectedIndex !== -1) {
+      // Remove the selected option
+      select.remove(selectedIndex);
+    }
+  });
+};
